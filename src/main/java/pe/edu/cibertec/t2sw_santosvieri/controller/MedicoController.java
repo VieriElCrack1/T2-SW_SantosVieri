d29049a018deb233c12771c64e3160d81a5dcc84
VieriElCrack1 <santosvieri9@gmail.com>
2024-12-08 18:07:10 -0500

    terminado registrar medico POST

--- src/main/java/pe/edu/cibertec/t2sw_santosvieri/controller/MedicoController.java
+++ src/main/java/pe/edu/cibertec/t2sw_santosvieri/controller/MedicoController.java
@@ -1,10 +1,13 @@
 package pe.edu.cibertec.t2sw_santosvieri.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.t2sw_santosvieri.model.dto.request.MedicoRequest;
 import pe.edu.cibertec.t2sw_santosvieri.model.dto.response.MedicoResponse;
+import pe.edu.cibertec.t2sw_santosvieri.model.dto.response.Respuesta;
 import pe.edu.cibertec.t2sw_santosvieri.service.IMedicoService;
 
 import java.util.List;
@@ -34,5 +37,27 @@ public class MedicoController {
     }
 
     //registrar medico
-
+    @PostMapping
+    public ResponseEntity<Respuesta<MedicoResponse>> guardarMedico(@RequestBody MedicoRequest request) {
+        try {
+            MedicoResponse medico = medicoService.guardarMedico(request);
+            Respuesta<MedicoResponse> respuesta = Respuesta.<MedicoResponse>builder()
+                    .mensaje("Medico registrado exitosamente")
+                    .data(medico)
+                    .build();
+            return new ResponseEntity<>(respuesta, HttpStatus.CREATED);
+        }catch (DataAccessException e) {
+            Respuesta<MedicoResponse> respuesta = Respuesta.<MedicoResponse>builder()
+                    .mensaje("No se pudo acceder a la base de datos")
+                    .errorMensaje(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                    .build();
+            return new ResponseEntity<>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+        }catch (Exception e) {
+            Respuesta<MedicoResponse> respuesta = Respuesta.<MedicoResponse>builder()
+                    .mensaje("No se realizo el registro del medico")
+                    .errorMensaje(HttpStatus.BAD_REQUEST.name())
+                    .build();
+            return new ResponseEntity<>(respuesta, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
