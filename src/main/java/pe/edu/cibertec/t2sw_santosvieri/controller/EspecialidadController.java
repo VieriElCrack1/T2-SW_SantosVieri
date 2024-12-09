ee9e3f65e53e5a49fb3ff1aa4f5abce899117a67
VieriElCrack1 <santosvieri9@gmail.com>
2024-12-08 19:11:08 -0500

    terminado registrar especialidad POST

--- src/main/java/pe/edu/cibertec/t2sw_santosvieri/controller/EspecialidadController.java
+++ src/main/java/pe/edu/cibertec/t2sw_santosvieri/controller/EspecialidadController.java
@@ -1,13 +1,13 @@
 package pe.edu.cibertec.t2sw_santosvieri.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pe.edu.cibertec.t2sw_santosvieri.model.dto.request.EspecialidadRequest;
 import pe.edu.cibertec.t2sw_santosvieri.model.dto.response.EspecialidadResponse;
+import pe.edu.cibertec.t2sw_santosvieri.model.dto.response.Respuesta;
 import pe.edu.cibertec.t2sw_santosvieri.service.IEspecialidadService;
 
 import java.util.List;
@@ -36,4 +36,27 @@ public class EspecialidadController {
     }
 
     //registrar especialidad
+    @PostMapping
+    public ResponseEntity<Respuesta<EspecialidadResponse>> registrarEspecialidad(@RequestBody EspecialidadRequest request) {
+        try{
+            EspecialidadResponse especialidad = especialidadService.guardarEspecialidad(request);
+            Respuesta<EspecialidadResponse> respuesta = Respuesta.<EspecialidadResponse>builder()
+                    .mensaje("Especialidad registrado exitosamente")
+                    .data(especialidad)
+                    .build();
+            return new ResponseEntity<>(respuesta, HttpStatus.CREATED);
+        }catch (DataAccessException e) {
+            Respuesta<EspecialidadResponse> respuesta = Respuesta.<EspecialidadResponse>builder()
+                    .mensaje("No se pudo acceder a la base de datos")
+                    .errorMensaje(HttpStatus.INTERNAL_SERVER_ERROR.name())
+                    .build();
+            return new ResponseEntity<>(respuesta, HttpStatus.INTERNAL_SERVER_ERROR);
+        }catch (Exception e) {
+            Respuesta<EspecialidadResponse> respuesta = Respuesta.<EspecialidadResponse>builder()
+                    .mensaje("No se realizo el registro de especialidad")
+                    .errorMensaje(HttpStatus.BAD_REQUEST.name())
+                    .build();
+            return new ResponseEntity<>(respuesta, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
